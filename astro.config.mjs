f9cd05723cbce2d048de857dee86e907213c6788
Christine Tham <chris.tham@hellotham.com>
2023-02-08 20:19:42 +1100

    Cleaned config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,5 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-      external: ['svgo'],
-    },
-  },
-  site: 'https://hellotham.github.io',
   integrations: [tailwind()],
 })
