e8ecdcfa18868ee77e2e1781d276cd853cacaf2b
Christine Tham <chris.tham@hellotham.com>
2023-04-07 09:21:11 +1000

    Filter draft posts in getCollection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,9 @@ import { getCollection } from 'astro:content'
 import { SiteMetadata } from '../config'
 
 export async function get() {
-  const posts = await getCollection('blog')
+  const posts = await getCollection('blog', ({ data }) => {
+    return data.draft !== true;
+  })
   return rss({
     // `<title>` field in output xml
     title: SiteMetadata.title,
@@ -15,7 +17,7 @@ export async function get() {
     // list of `<item>`s in output xml
     // simple example: generate items for every md file in /src/pages
     // see "Generating items" section for required data and advanced use cases
-    items: posts.filter(post => !post.data.draft).map(post => ({
+    items: posts.map(post => ({
       link: import.meta.env.BASE_URL + '/blog/' + post.slug,
       title: post.data.title,
       description: post.data.description,
