de7e18a46676601214365348ca13dc0c1f9b763d
Christine Tham <chris.tham@hellotham.com>
2023-04-09 17:53:02 +1000

    New Astro logo

--- src/config.ts
+++ src/config.ts
@@ -69,8 +69,8 @@ export const SiteMetadata = {
   buildTime: new Date(),
 }
 
-export {default as Logo} from './assets/astro/logomark-light.png'
-export {default as LogoImage} from './assets/astro/full-logo-light.png'
+export {default as Logo} from './assets/svg/astro/astro-icon-dark.svg'
+export {default as LogoImage} from './assets/astro/astro-logo-dark.png'
 export {default as FeaturedSVG} from './assets/svg/undraw/undraw_design_inspiration.svg'
 export {default as DefaultSVG} from './assets/svg/undraw/undraw_my_feed.svg'
 export {default as DefaultImage} from './assets/undraw/undraw_my_feed.png'
