09a2fead3826fb48fd1ea40063ea56df1e091146
Alejandro Vargas <alejo@me.com>
2023-06-21 07:39:32 -0400

    build: Initial stable build

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,11 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp'),
-  ],
+    require('@tailwindcss/aspect-ratio')
+  ]
 }
