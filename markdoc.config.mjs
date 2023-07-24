d1e9ff343f58cccef3071b28c6a4d99ed719f650
Christine Tham <chris.tham@hellotham.com>
2023-07-25 09:06:34 +1000

    Change case of gallery.astro in markdoc config

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -3,7 +3,7 @@ import { defineMarkdocConfig, component } from '@astrojs/markdoc/config';
 export default defineMarkdocConfig({
   tags: {
     Gallery: {
-      render: component('./src/components/Gallery.astro'),
+      render: component('./src/components/gallery.astro'),
       attributes: {
         folder: { type: String },
       }
